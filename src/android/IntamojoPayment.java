d34d44e280627fa8b8ff0085c96cc00876a0f644
Devesh mishra <deveshmishra34@gmail.com>
2018-09-30 14:22:22 +0530

    intent to json value

--- src/android/IntamojoPayment.java
+++ src/android/IntamojoPayment.java
@@ -60,7 +60,7 @@ public class IntamojoPayment extends CordovaPlugin {
             String status = extras.getString("status"); // data parameter will be send from the other activity.
             if(orderId != null && status != null){
                 String information = "{'orderId': '" + orderId + "', 'status': '" + status + "'}";
-                PluginResult result = new PluginResult(PluginResult.Status.OK, information);
+                PluginResult result = new PluginResult(PluginResult.Status.OK, getIntentJson(data));
                 result.setKeepCallback(true);
                 PUBLIC_CALLBACKS.sendPluginResult(result);
             }else{
@@ -71,6 +71,117 @@ public class IntamojoPayment extends CordovaPlugin {
             }
             
         // }
+    }
+
+    private JSONObject getIntentJson(Intent intent) {
+        JSONObject intentJSON = null;
+        ClipData clipData = null;
+        JSONObject[] items = null;
+        ContentResolver cR = this.cordova.getActivity().getApplicationContext().getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            clipData = intent.getClipData();
+            if (clipData != null) {
+                int clipItemCount = clipData.getItemCount();
+                items = new JSONObject[clipItemCount];
+
+                for (int i = 0; i < clipItemCount; i++) {
+
+                    ClipData.Item item = clipData.getItemAt(i);
+
+                    try {
+                        items[i] = new JSONObject();
+                        items[i].put("htmlText", item.getHtmlText());
+                        items[i].put("intent", item.getIntent());
+                        items[i].put("text", item.getText());
+                        items[i].put("uri", item.getUri());
+
+                        if (item.getUri() != null) {
+                            String type = cR.getType(item.getUri());
+                            String extension = mime.getExtensionFromMimeType(cR.getType(item.getUri()));
+
+                            items[i].put("type", type);
+                            items[i].put("extension", extension);
+                        }
+
+                    } catch (JSONException e) {
+                        Log.d(LOG_TAG, " Error thrown during intent > JSON conversion");
+                        Log.d(LOG_TAG, e.getMessage());
+                        Log.d(LOG_TAG, Arrays.toString(e.getStackTrace()));
+                    }
+
+                }
+            }
+        }
+
+        try {
+            intentJSON = new JSONObject();
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                if (items != null) {
+                    intentJSON.put("clipItems", new JSONArray(items));
+                }
+            }
+
+            intentJSON.put("type", intent.getType());
+            intentJSON.put("extras", toJsonObject(intent.getExtras()));
+            intentJSON.put("action", intent.getAction());
+            intentJSON.put("categories", intent.getCategories());
+            intentJSON.put("flags", intent.getFlags());
+            intentJSON.put("component", intent.getComponent());
+            intentJSON.put("data", intent.getData());
+            intentJSON.put("package", intent.getPackage());
+
+            return intentJSON;
+        } catch (JSONException e) {
+            Log.d(LOG_TAG, " Error thrown during intent > JSON conversion");
+            Log.d(LOG_TAG, e.getMessage());
+            Log.d(LOG_TAG, Arrays.toString(e.getStackTrace()));
+
+            return null;
+        }
+    }
+
+    private static JSONObject toJsonObject(Bundle bundle) {
+        // Credit: https://github.com/napolitano/cordova-plugin-intent
+        try {
+            return (JSONObject) toJsonValue(bundle);
+        } catch (JSONException e) {
+            throw new IllegalArgumentException("Cannot convert bundle to JSON: " + e.getMessage(), e);
+        }
+    }
+
+    private static Object toJsonValue(final Object value) throws JSONException {
+        // Credit: https://github.com/napolitano/cordova-plugin-intent
+        if (value == null) {
+            return null;
+        } else if (value instanceof Bundle) {
+            final Bundle bundle = (Bundle) value;
+            final JSONObject result = new JSONObject();
+            for (final String key : bundle.keySet()) {
+                result.put(key, toJsonValue(bundle.get(key)));
+            }
+            return result;
+        } else if ((value.getClass().isArray())) {
+            final JSONArray result = new JSONArray();
+            int length = Array.getLength(value);
+            for (int i = 0; i < length; ++i) {
+                result.put(i, toJsonValue(Array.get(value, i)));
+            }
+            return result;
+        } else if (value instanceof ArrayList<?>) {
+            final ArrayList arrayList = (ArrayList<?>) value;
+            final JSONArray result = new JSONArray();
+            for (int i = 0; i < arrayList.size(); i++)
+                result.put(toJsonValue(arrayList.get(i)));
+            return result;
+        } else if (value instanceof String || value instanceof Boolean || value instanceof Integer
+                || value instanceof Long || value instanceof Double) {
+            return value;
+        } else {
+            return String.valueOf(value);
+        }
     }
 
 }
